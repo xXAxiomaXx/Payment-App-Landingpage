cdabc26a5d4cd91d71b546964b23cb78dd888319
Eric Moura <100503308+xXAxiomaXx@users.noreply.github.com>
2024-05-08 17:37:43 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Route path='/' element={<Main/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/register' element={<Register/>} />
+      <Route path='/checkout' element={<Checkout/>} />
       </Routes>
       </BrowserRouter>
   );
