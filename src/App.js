e64aa212d7ad3d2c74a53c653249d234e17eb620
Eric Moura <100503308+xXAxiomaXx@users.noreply.github.com>
2024-05-08 17:35:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
       <Route path='/' element={<Main/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/register' element={<Register/>} />
-      <Route path='/checkout' element={<Checkout/>} />
       </Routes>
       </BrowserRouter>
   );
