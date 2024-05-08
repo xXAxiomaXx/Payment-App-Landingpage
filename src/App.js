e28486e2a10727c2086e7233d9b3ba6fe80d976a
Eric Moura <100503308+xXAxiomaXx@users.noreply.github.com>
2024-05-08 17:40:10 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Main from './Components/Pages/Main';
 import Login from './Components/Pages/Login';
 import Register from './Components/Pages/Register';
-import Checkout from './Components/Pages/Checkout';
 
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <Route path='/' element={<Main/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/register' element={<Register/>} />
-      <Route path='/checkout' element={<Checkout/>} />
       </Routes>
       </BrowserRouter>
   );
